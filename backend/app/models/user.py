c5afe8e0fe27eda5b8f7c9155523c66aab15cada
kunalkosh2004 <harshkosh200407@gmail.com>
2026-08-01 23:12:50 +0530

    user model created

---
+++ backend/app/models/user.py
@@ -0,0 +1,45 @@
+from app.db.base import Base
+from sqlalchemy import Boolean, DateTime, String
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.dialects.postgresql import UUID
+from geoalchemy2 import Geography
+from datetime import UTC, datetime
+from geoalchemy2.elements import WKBElement
+import uuid
+
+
+TIMESTAMP = DateTime(timezone=True)
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        nullable=False
+    )
+    nickname: Mapped[str] = mapped_column(String(30), nullable=False)
+    location: Mapped[WKBElement] = mapped_column(
+        Geography("POINT", srid=4326),
+        nullable=False,
+    )
+    is_online: Mapped[bool] = mapped_column(
+        Boolean,
+        default=False, 
+        server_default="false",
+        nullable=False
+    )
+    last_seen: Mapped[datetime | None] = mapped_column(
+        TIMESTAMP,
+        nullable=True,
+    )
+    created_at: Mapped[datetime] = mapped_column(
+        TIMESTAMP, default=lambda: datetime.now(UTC), nullable=False
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        TIMESTAMP,
+        default=lambda: datetime.now(UTC),
+        onupdate=lambda: datetime.now(UTC),
+        nullable=False,
+    )
